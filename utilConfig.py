e7f996884779a0a2b46fa72ef879cdc2a6d014d4
Ferabeo <snak.st@gmail.com>
2021-04-25 20:36:06 +0200

    Fix path to config file

--- utilConfig.py
+++ utilConfig.py
@@ -1,9 +1,13 @@
-import confuse 
+import confuse
+import os
+import sys
 
-appName='RSSDownloader'
-appConfig='config.yaml'
+
+appName = 'RSSDownloader'
+appConfig = 'config.yaml'
+
 
 def getConfig():
     config = confuse.Configuration(appName, __name__)
-    config.set_file(appConfig)
+    config.set_file(os.path.join(sys.path[0], appConfig))
     return config
